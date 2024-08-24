84c29a17039cf3cffa4ab7fe935e23492a13fc43
abood77-sstak <omar7mw@gmail.com>
2024-08-24 06:59:54 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+
+import os
+from telethon import TelegramClient, events
+
+API_ID = os.environ("api_id")
+API_HASH = os.environ("api_hash")
+BOT_TOKEM = os.environ("token_bot")
+
+
+client = TelegramClient ("mbot", API_ID, API_HASH)
+client.start(bot_token=BOT_TOKEM)
+@client.on(events.NewMessage)
+async def handler (event):
+        await event.reply(event.raw_text)
+
+client.start()
+client.run_until_disconnected()
+
+
